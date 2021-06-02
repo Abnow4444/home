2fc91641738e267d9c935db4eec89733e3454694
Abhinav Singirthi <75773579+Abnow4444@users.noreply.github.com>
2021-06-02 04:56:21 -0700

    Modified for Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { ThoughtsComponent } from './thoughts/thoughts.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
+  // {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'thoughts', component: ThoughtsComponent}
 ];
 
