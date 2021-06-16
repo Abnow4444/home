b7b85aa27b76a21e62505141b2d3dc172eef2077
Abhinav Singirthi <75773579+Abnow4444@users.noreply.github.com>
2021-06-16 10:25:55 -0700

    Modified for Experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
 import { HomeComponent } from './home/home.component';
 import { ThoughtsComponent } from './thoughts/thoughts.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   // {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'thoughts', component: ThoughtsComponent}
+  {path: 'thoughts', component: ThoughtsComponent},
+  {path: 'experience', component: ExperienceComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ThoughtsComponent } from './thoughts/thoughts.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ThoughtsComponent
+    ThoughtsComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
