6e9078bc14aa6ed052f8ba7af83f2eca8799aeeb
Abhinav Singirthi <75773579+Abnow4444@users.noreply.github.com>
2021-06-25 09:42:08 -0700

    Modified for Visitor Component

--- src/app/user-details.service.ts
+++ src/app/user-details.service.ts
@@ -6,8 +6,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class UserDetailsService implements OnInit{
 
-  // url:any = 'https://jsonip.com/';
-  url: any = 'http://www.geoplugin.net/json.gp';
+  // url: any = 'http://www.geoplugin.net/json.gp';
+  url: any = 'https://jsonip.com/';
   visitorUrl: any = 'https://sheet.best/api/sheets/a50580bd-4e7c-489a-9cf5-2072d03d71ff';
 
   constructor(private http: HttpClient) { }
@@ -20,8 +20,11 @@ export class UserDetailsService implements OnInit{
     return this.http.get(this.url);
   }
 
-  updateDetails(city: any, continent: any, country: any, latitude: any, longitude: any, state: any, ip: any){
-    this.http.post(this.visitorUrl, {city: city, continent: continent, country: country, latitude: latitude, longitude: longitude, state: state, ip: ip}, {headers : new HttpHeaders({ 'Content-Type': 'application/json' })}).subscribe();
+  // updateDetails(city: any, continent: any, country: any, latitude: any, longitude: any, state: any, ip: any){
+  //   this.http.post(this.visitorUrl, {city: city, continent: continent, country: country, latitude: latitude, longitude: longitude, state: state, ip: ip}, {headers : new HttpHeaders({ 'Content-Type': 'application/json' })}).subscribe();
+  // }
+  updateDetails(ip: any){
+    this.http.post(this.visitorUrl, {ip: ip}, {headers : new HttpHeaders({ 'Content-Type': 'application/json' })}).subscribe();
   }
 
   retrieveUserDBDetails(){
--- src/app/visitor/visitor.component.ts
+++ src/app/visitor/visitor.component.ts
@@ -10,15 +10,44 @@ import { UserObj } from './UserObj';
 export class VisitorComponent implements OnInit {
 
   visitorData : any;
-  constructor(private userDetails: UserDetailsService) { }
+  userObj: any;
+  xObj: any;
+  checkUserObj: any;
+  len: number = 0;
+
+  constructor(private userDetails: UserDetailsService, private userService: UserDetailsService) { }
+
 
   ngOnInit(): void {
-    const sub = this.userDetails.retrieveUserDBDetails().subscribe({
-      next: data => {
-        this.visitorData = data;
-      },
-      error: err => console.log(err)
-    })
+    this.updateDetails();
+  }
+
+  updateDetails(): void{
+    this.userService.getUserDetails().subscribe(
+      {
+        next: res => {
+          this.checkUserObj = this.userService.retrieveUserDBDetails().subscribe({
+            next: data=>{
+              this.checkUserObj = data;
+              this.userObj = res;
+              this.len = this.checkUserObj.filter((x: UserObj)  => x.ip == this.userObj.ip).length;
+              console.log(this.len);
+              if(this.len === 0){
+                this.userService.updateDetails(this.userObj.ip);
+                // this.userService.updateDetails(this.userObj.geoplugin_city, this.userObj.geoplugin_continentName, this.userObj.geoplugin_countryName, this.userObj.geoplugin_latitude, this.userObj.geoplugin_longitude, this.userObj.geoplugin_region, this.userObj.geoplugin_request)
+              }
+              const sub = this.userDetails.retrieveUserDBDetails().subscribe({
+                next: data => {
+                  this.visitorData = data;
+                },
+                error: err => console.log(err)
+              })
+            },
+            error: err => console.log(err)
+          });
+        },
+        error: err => console.log(err)
+      })
   }
 
 }
